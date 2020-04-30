e5afaf0bb8c65f7d1ca80a5b2afba49f3167f4c1
zz-big <58782753+zz-big@users.noreply.github.com>
2020-04-30 08:28:38 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -15,5 +15,6 @@ public class Test {
         System.out.println("hello");
         System.out.println();
         System.out.println("asdfasdfsd");
+        System.out.println("asdfasdfsd");
     }
 }
